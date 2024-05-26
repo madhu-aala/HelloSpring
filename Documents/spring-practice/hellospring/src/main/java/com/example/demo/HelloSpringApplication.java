4a9e90cfa3865c00d58aeba88762269b5d47c690
madhu-aala <aalamadhu7@gmail.com>
2024-05-26 18:53:40 +0530

    Initial commit.

---
+++ Documents/spring-practice/hellospring/src/main/java/com/example/demo/HelloSpringApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(HelloSpringApplication.class, args);
+		
+		Hello obj = context.getBean(Hello.class);
+		
+		obj.sayHello();
+	}
+
+}
