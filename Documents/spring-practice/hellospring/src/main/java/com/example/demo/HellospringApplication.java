4d5c0b56b5ccced833263a129b2d9caa1856eba3
Madhu <Madhu@Madhu>
2024-05-26 20:57:34 +0530

    Test

---
+++ Documents/spring-practice/hellospring/src/main/java/com/example/demo/HellospringApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class HellospringApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(HellospringApplication.class, args);
+		Hello obj = context.getBean(Hello.class);
+		obj.sayHello();
+	}
+
+}
